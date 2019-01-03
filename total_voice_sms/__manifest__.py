6a68cfb2db15aa2b04c2d8fb407cc3fca9f37974
Danimar Ribeiro <danimaribeiro@gmail.com>
2019-01-03 19:56:51 -0200

    [ADD] Módulo base inicial

---
+++ total_voice_sms/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2016 Danimar Ribeiro <danimaribeiro@gmail.com>, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Envio de SMS',
+    'summary': "Envio de SMS",
+    'version': '11.0.1.0.0',
+    'category': 'account',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+    ],
+    'depends': [
+    ],
+    'data': [
+    ],
+}
