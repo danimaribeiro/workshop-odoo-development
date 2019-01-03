17765f23055d0f98175065209b11fccd0f5bcfe4
Danimar Ribeiro <danimaribeiro@gmail.com>
2019-01-03 21:00:50 -0200

    [ADD] Melhoria na interface do sms

--- total_voice_sms/models/sms.py
+++ total_voice_sms/models/sms.py
@@ -1,6 +1,10 @@
 
+import json
+from datetime import datetime
 from odoo import fields, models
 from odoo.exceptions import UserError
+
+from totalvoice.cliente import Cliente
 
 
 class TotalVoiceSMS(models.Model):
@@ -10,6 +14,21 @@ class TotalVoiceSMS(models.Model):
     number_to = fields.Char()
     message = fields.Char()
     send_date = fields.Datetime()
+    state = fields.Selection(
+        [('draft', 'Provisório'),
+         ('sent', 'Enviado')], default="draft")
 
     def action_enviar_sms(self):
-        raise UserError('Já vou implementar')
+        cliente = Cliente("571058a184ef75e9b249216e56421c7c",
+                          'api.totalvoice.com.br')
+
+        numero_destino = self.number_to
+        mensagem = self.message
+        response = cliente.sms.enviar(numero_destino, mensagem)
+
+        resposta = json.loads(response)
+        if resposta['sucesso']:
+            self.send_date = datetime.now()
+            self.state = 'sent'
+        else:
+            raise UserError(resposta['mensagem'])
